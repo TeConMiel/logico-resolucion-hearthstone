4dac99dae25378000f508888fa521c8df04ddb69
TeConMiel <fromagnoli@frba.utn.edu.ar>
2021-08-21 04:16:54 -0300

    Realice punto 1

--- resolucion.pl
+++ resolucion.pl
@@ -5,4 +5,41 @@ Alumno: Franco damian romagnoli
 Legajo: 173.112-9
 */
 
+jugador(jugador(franco, 100, 100, [prueba1], [prueba2], [prueba3])).
 
+nombre(jugador(Nombre,_,_,_,_,_), Nombre).
+nombre(criatura(Nombre,_,_,_), Nombre).
+nombre(hechizo(Nombre,_,_), Nombre).
+
+vida(jugador(_,Vida,_,_,_,_), Vida).
+vida(criatura(_,_,Vida,_), Vida).
+vida(hechizo(_,curar(Vida),_), Vida).
+
+danio(criatura(_,Danio,_), Danio).
+danio(hechizo(_,danio(Danio),_), Danio).
+
+mana(jugador(_,_,Mana,_,_,_), Mana).
+mana(criatura(_,_,_,Mana), Mana).
+mana(hechizo(_,_,Mana), Mana).
+
+cartasMazo(jugador(_,_,_,Cartas,_,_), Cartas).
+cartasMano(jugador(_,_,_,_,Cartas,_), Cartas).
+cartasCampo(jugador(_,_,_,_,_,Cartas), Cartas).
+
+%-------[PUNTO 1]--------%
+
+tieneCarta(Jugador, Carta) :-
+    jugador(Jugador),
+    cartasPoseidas(Jugador, CartasPoseidas),
+    member(Carta, CartasPoseidas).
+
+cartasPoseidas(Jugador, Cartas) :-
+    cartasMazo(Jugador, Cartas).
+
+cartasPoseidas(Jugador, Cartas) :-
+    cartasMano(Jugador, Cartas).
+
+cartasPoseidas(Jugador, Cartas) :-
+    cartasCampo(Jugador, Cartas).
+
+%-------[PUNTO 2]--------%
